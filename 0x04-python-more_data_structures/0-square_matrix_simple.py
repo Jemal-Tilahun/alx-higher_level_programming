fc200744fae3be6f334dcf2646b0ab6a38122ec4
Jemal <jemaltilahun14@gmail.com>
2022-12-09 20:38:02 +0000

    ALL TASKS

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,10 +2,3 @@
 
 def square_matrix_simple(matrix=[]):
     return [list(map(lambda x: x ** 2, row)) for row in matrix]
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
